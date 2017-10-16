fd86fb6cd30012ade0b4bc0bdc6ed4d1009111ed
ccm36 <cameron.c.malloy@gmail.com>
2017-10-16 16:25:00 -0500

    Pass all tests

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -5,7 +5,8 @@ var Movie = Backbone.Model.extend({
   },
 
   toggleLike: function() {
-    // your code here
+    var isLiked = this.get('like');  
+    this.set('like', !isLiked);
   }
 
 });
@@ -15,13 +16,16 @@ var Movies = Backbone.Collection.extend({
   model: Movie,
 
   initialize: function() {
-    // your code here
+    this.on('change:like', function () {
+      this.sort();
+    }, this);
   },
 
   comparator: 'title',
 
   sortByField: function(field) {
-    // your code here
+    this.comparator = field;
+    this.sort();
   }
 
 });
@@ -58,7 +62,9 @@ var MovieView = Backbone.View.extend({
                         </div>'),
 
   initialize: function() {
-    // your code here
+    this.model.on('change', function() {
+      this.render();
+    }, this);
   },
 
   events: {
@@ -66,7 +72,7 @@ var MovieView = Backbone.View.extend({
   },
 
   handleClick: function() {
-    // your code here
+    this.model.toggleLike();
   },
 
   render: function() {
@@ -79,7 +85,9 @@ var MovieView = Backbone.View.extend({
 var MoviesView = Backbone.View.extend({
 
   initialize: function() {
-    // your code here
+    this.collection.on('sort', function () {
+      this.render();
+    }, this) 
   },
 
   render: function() {
